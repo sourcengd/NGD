759dfff9f39ab3d5faf55ec40a6e83c52c055e0a
sourcengd <basharseed126@gmail.com>
2024-05-11 20:35:16 +0300

    Update zzcmd.py

--- AarohiX/plugins/play/zzcmd.py
+++ AarohiX/plugins/play/zzcmd.py
@@ -1,13 +1,8 @@
-import asyncio
 from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from strings.filters import command
-from pyrogram.types import InlineKeyboardButton, CallbackQuery, InlineKeyboardMarkup, Message
-from typing import Union
-from pyrogram.types import InlineKeyboardButton
-from ZeMusic import app
-from ZeMusic.misc import HAPP, SUDOERS, XCB
-from config import OWNER_ID
-import config                                       
+from AarohiX import app
+import config
                                        
 @app.on_callback_query(filters.regex("zzzback"))
 async def zzzback(_, query: CallbackQuery):
