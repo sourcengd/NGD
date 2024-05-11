3d0c0d3db45d3515cc6eb066dd2a07f070d4bbe0
sourcengd <basharseed126@gmail.com>
2024-05-11 18:09:49 +0300

    Update config.py

--- config.py
+++ config.py
@@ -39,7 +39,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/sourcengd",
+    "https://github.com/sourcengd/NGD",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
